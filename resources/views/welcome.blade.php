59a8e1afe3b855106024af5f89d9aa3a082b16ef
imsahilsharma <sahil411sharma@gmail.com>
2021-03-17 11:57:54 +0530

    LP 1.0

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Student Login</title>
+</head>
+<body>
+<div class="container">
+    <h1 style="text-align: center;">SIGNUP</h1>
+    <div class="row">
+    <div class="col col-0 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div>
+    <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
+        <table class="table table-borderless">
+            <tr>
+                <td>Name</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Roll No</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>Admission No</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>College</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button class="btn btn-primary">SUBMIT</button></td>
+            </tr>
+        </table>
+    </div>
+    <div class="col col-0 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div></div>
+</div>
+    
+</body>
+</html>
