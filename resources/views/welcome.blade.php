1a66e0a5d5e45036c5ca3082feb8744579fe223d
imsahilsharma <sahil411sharma@gmail.com>
2021-03-17 12:48:19 +0530

    LP 2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,28 @@
     <title>Student Login</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">RCSS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/faculty">Faculty</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+     </ul>
+    </div>
+  </div>
+</nav>
+
 <div class="container">
     <h1 style="text-align: center;">SIGNUP</h1>
     <div class="row">
@@ -38,6 +60,6 @@
     </div>
     <div class="col col-0 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"></div></div>
 </div>
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
